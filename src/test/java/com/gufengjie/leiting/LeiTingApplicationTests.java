1508448f4f0f42f76f1b23300b57a454d89b3b56
古丰杰 <gfjack321@gmail.com>
2025-06-17 17:10:18 +0800

    项目运营

---
+++ src/test/java/com/gufengjie/leiting/LeiTingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gufengjie.leiting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeiTingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
