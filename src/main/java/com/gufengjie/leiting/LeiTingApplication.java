c9e5c380c5d5aaf678f50f1ea871f310e5c41f57
古丰杰 <gfjack321@gmail.com>
2025-06-17 17:10:12 +0800

    项目运营

---
+++ src/main/java/com/gufengjie/leiting/LeiTingApplication.java
@@ -0,0 +1,13 @@
+package com.gufengjie.leiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeiTingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeiTingApplication.class, args);
+    }
+
+}
